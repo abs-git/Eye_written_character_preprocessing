f17bb22381dd49250bf22f71a2e3e048ae853fea
donghyun kang <48580174+abs-git@users.noreply.github.com>
2022-12-16 17:32:42 +0900

    fix

---
+++ eog_classification_v2/experiments/data_handler.py
@@ -0,0 +1,123 @@
+
+from collections import defaultdict
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def train_test_split(data_dict, test_rate=0.2):
+    classes = list(data_dict.keys())
+
+    train_set = defaultdict(list)
+    test_set = defaultdict(list)
+    for cls in classes:
+        values = np.array(data_dict[cls])
+
+        n_test = int(len(values)*test_rate)
+        n_train = int(len(values)-n_test)
+
+        train_indices = np.random.choice(len(values),n_train,replace=False)
+        test_indices = np.setdiff1d(range(len(values)), train_indices)
+
+        train_set[int(cls)] = values[train_indices, :]
+        test_set[int(cls)] = values[test_indices, :]
+
+    return train_set, test_set
+
+
+def get_train_batch(data_dict, batch_size, n_batch, ref_dict=None, ref_key=None):
+    classes = list(data_dict.keys())
+
+    train_batch = []
+    train_targets = []
+    for n in range(n_batch):
+        cls = np.random.randint(low=min(classes), high=max(classes), size=batch_size//2).tolist()
+        pos_cls = cls
+        neg_cls = [np.random.choice(np.setdiff1d(classes,cls[i]), size=1).item() for i in range(batch_size//2)]
+
+        targets = np.ones((batch_size,1))
+        targets[batch_size // 2:] = 0
+
+        values = []
+        pos_values = []
+        neg_values = []
+        for i in range(batch_size//2):
+
+            value_idx = np.random.choice(len(data_dict[cls[i]]), size=1).item()
+            pos_idx = np.random.choice(len(data_dict[pos_cls[i]]), size=1).item()
+            neg_idx = np.random.choice(len(data_dict[neg_cls[i]]), size=1).item()
+            
+            value = data_dict[cls[i]][value_idx]
+            if ref_dict:
+                # with reference data
+                if n%2==0:
+                    pos_value = np.array(ref_dict[ref_key][str(pos_cls[i])]).squeeze(axis=0)
+                    neg_value = np.array(ref_dict[ref_key][str(neg_cls[i])]).squeeze(axis=0)
+                else:
+                    pos_value = data_dict[pos_cls[i]][pos_idx]
+                    neg_value = data_dict[neg_cls[i]][neg_idx]
+            else:
+                # only real data
+                pos_value = data_dict[pos_cls[i]][pos_idx]
+                neg_value = data_dict[neg_cls[i]][neg_idx]
+
+            values.append(value)
+            pos_values.append(pos_value)
+            neg_values.append(neg_value)
+
+        anchors = np.concatenate([values,values])
+        comparison = np.concatenate([pos_values,neg_values])
+        pairs = list(np.array([anchors, comparison]))
+
+        train_batch.append(pairs)             # (n_batch, pairs, batch_size, length, point)
+        train_targets.append(targets)         # (n_batch, target)
+
+    return train_batch, train_targets
+
+
+
+def get_test_batch(data_dict, ref_dict, ref_key):
+    classes = list(data_dict.keys())
+    n_class = len(classes)
+
+    test_batch = []
+    test_targets = []
+    for cls in classes:
+        for value in data_dict[cls]:
+            anchors = np.array([value]*n_class)
+            comparison = np.array(list(ref_dict[ref_key].values())).squeeze(axis=1)
+            
+            pairs = list(np.array([anchors, comparison]))
+            targets = np.zeros((n_class,1))
+            if ref_key == 'katakana':
+                targets[cls-1] = 1
+            else:
+                targets[cls] = 1
+
+            test_batch.append(pairs)
+            test_targets.append(targets)
+
+    return test_batch, test_targets
+
+
+def plot_images(input_list, task='train'):
+  row, col, _, _ = np.array(input_list).shape
+  fig,axes = plt.subplots(row,col, figsize=(col//row*4,row*2))
+  n = 0
+  for r in range(row):
+    for c in range(col):
+      x,y = zip(*input_list[r][c])
+
+      axes[r][c].set_xlim([-0.2, 1.2])
+      axes[r][c].set_ylim([-0.2, 1.2])
+      axes[r][c].get_xaxis().set_visible(False)
+      axes[r][c].get_yaxis().set_visible(False)
+
+      if task=='test':
+        axes[r][c].scatter(x,y, 1)
+      else:
+        if c >= col//2:
+          axes[r][c].scatter(x,y, 1, color='red')
+        else:
+          axes[r][c].scatter(x,y, 1, color='blue')
+  n += 1
---
+++ eog_classification_v2/experiments/utils.py
@@ -0,0 +1,77 @@
+import numpy as np
+import data_handler as dh
+import models
+from tensorflow.keras.optimizers import Adam
+
+
+def learn(model, train_batch, train_targets):    
+    acc = 0.0
+    loss = 0.0
+    i = 0
+    for batch in zip(train_batch, train_targets):
+        data, target = batch
+
+        batch_metrics = model.train_on_batch(data, target)
+
+        acc += batch_metrics[1]
+        loss += batch_metrics[0]
+        i+=1
+
+    acc = (acc/i)*100
+    loss = (loss/i)
+    return acc, loss
+
+def evaluate(model, test_batch, test_targets):
+    correct = 0
+    i = 0
+    for batch in zip(test_batch, test_targets):
+        data, target = batch
+
+        probs = model.predict_on_batch(data)
+        
+        if np.argmax(probs) == np.argmax(target):
+            correct +=1
+        i+=1
+
+    acc = (correct/i)*100
+    return acc
+
+
+def experiment(cfg, real_data, reference_data):
+
+    # data setting
+    train_dict, test_dict = dh.train_test_split(real_data, cfg.split_ratio)
+    test_batch, test_targets = dh.get_test_batch(test_dict, reference_data, ref_key=cfg.ref_key)
+
+    # model setting
+    input_shape = test_batch[0][0][0].shape                # (length, points)
+
+    if cfg.model_type == 'HybridBaseModel':
+        base_model = models.HybridBaseModel(input_shape)
+    else:
+        base_model = models.ViTBaseModel(input_shape, cfg.ViT_params)
+
+    model = models.binary_siamese_net(input_shape, base_model.model())
+
+    # hyperparameters setting
+    optimizer = Adam(learning_rate = 0.001)
+    model.compile(loss = "binary_crossentropy", optimizer = optimizer, metrics = ['acc'])
+
+    # learn & evaluate
+    train_acc_list = []
+    train_loss_list = []
+    test_acc_list = []
+    for e in range(cfg.epochs):
+
+        train_batch, train_targets = dh.get_train_batch(train_dict, cfg.batch_size, cfg.n_batch, reference_data, ref_key=cfg.ref_key)
+        train_acc, train_loss = learn(model, train_batch, train_targets)
+        test_acc = evaluate(model, test_batch, test_targets)
+
+        train_acc_list.append(train_acc)
+        train_loss_list.append(train_loss)
+        test_acc_list.append(test_acc)
+
+        if e % 10 == 0:    
+            print('epoch : {}, train acc : {:.4f} %, train loss : {:.8f}, test acc : {:.4f} %, '.format(e+1, train_acc, train_loss, test_acc))
+    
+    return model, train_acc_list, train_loss_list, test_acc_list
