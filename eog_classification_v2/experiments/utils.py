a8465a2412c6d8720454f56e870d8658a8e2ceeb
donghyun kang <48580174+abs-git@users.noreply.github.com>
2023-02-04 14:53:03 +0900

    fix

--- eog_classification_v2/experiments/utils.py
+++ eog_classification_v2/experiments/utils.py
@@ -22,9 +22,10 @@ def learn(model, train_batch, train_targets):
     return acc, loss
 
 
-def evaluate(model, test_batch, test_targets):
+def evaluate(model, test_batch, test_targets, cm=False):
     correct = 0
     i = 0
+    confusion_matrix = np.zeros((len(test_targets[0]), len(test_targets[0])))
     for batch in zip(test_batch, test_targets):
         data, target = batch
 
@@ -34,11 +35,14 @@ def evaluate(model, test_batch, test_targets):
             correct +=1
         i+=1
 
+        if cm == True:
+            confusion_matrix[np.argmax(target)][np.argmax(probs)] += 1
+
     acc = (correct/i)*100
-    return acc
+    return acc, confusion_matrix
 
 
-def experiment(cfg, real_data, reference_data, zero_shot_cls=None):
+def experiment(cfg, real_data, reference_data, zero_shot_cls=None, cm=False):
 
     # data setting
     train_dict, test_dict = dh.train_test_split(real_data, cfg.split_ratio)
@@ -64,7 +68,7 @@ def experiment(cfg, real_data, reference_data, zero_shot_cls=None):
 
         train_batch, train_targets = dh.get_train_batch(train_dict, cfg.batch_size, cfg.n_batch, reference_data, ref_key=cfg.ref_key, zero_shot_cls=zero_shot_cls)
         train_acc, train_loss = learn(model, train_batch, train_targets)
-        test_acc = evaluate(model, test_batch, test_targets)
+        test_acc, _ = evaluate(model, test_batch, test_targets)
 
         train_acc_list.append(train_acc)
         train_loss_list.append(train_loss)
@@ -72,4 +76,10 @@ def experiment(cfg, real_data, reference_data, zero_shot_cls=None):
 
         print('epoch : {}, train acc : {:.4f} %, train loss : {:.8f}, test acc : {:.4f} %, '.format(e+1, train_acc, train_loss, test_acc))
     
+        if cm==True and e == cfg.epochs-1:
+            print('create confusion matrix')
+            _, confusion_matrix = evaluate(model, test_batch, test_targets, cm=True)
+                
+            return model, train_acc_list, train_loss_list, test_acc_list, confusion_matrix
+
     return model, train_acc_list, train_loss_list, test_acc_list
